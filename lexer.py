42a109a6434fa64163c3ce556c601615c4e64d3b
Bex Warner <rw1048a@student.american.edu>
2017-03-09 13:12:57 -0500

    added lexer comments and beginnings of interpretter

--- lexer.py
+++ lexer.py
@@ -1,7 +1,7 @@
 import re
-user_input = "function SquareDistance(x1, y1, x2, y2) { return x1^x2 + y1 ^ y2 } var distance = SquareDistance(2, 3, 5, 6) result = oldsum - value / 100 print result"
-output = ""
-# fuck
+
+user_input = "function SquareDistance(x1, y1, x2, y2){return x1^x2 + y1 ^ y2 } var distance = SquareDistance(2, 3, 5, 6) result = oldsum - value / 100 print result"
+
 base = ["=", "+", "-", "*", "/", "^", "(", ")", "{", "}", ",", ":"]
 
 known = {"var": "VAR", "print": "PRINT", "=": "ASSIGN", "return": "RETURN",
@@ -19,6 +19,12 @@ IDENT = re.compile("[a-zA-Z]+[a-zA-Z0-9_]*")
 user_input = user_input.split()
 output = ""
 
+'''
+    Checks if every word is a key word, if it is replace it with the
+    knownToIgnore version. If a word is not a key word,
+    it checks every chacter, searching for values in the base list,
+    changing it to the knownToIgnore version as well.
+'''
 for word in user_input:
     if word in known:
         output += known[word]
@@ -37,20 +43,27 @@ output = output.split()
 
 finaloutput = ""
 
+'''
+    Now that we know every (almost!) key word will be in knownToIgnore,
+    we can go through and check for idents and numbers.
+'''
+
 for term in output:
-    if term not in knownToIgnore:
+    # return has an edge case, so this is a final check
+    if term == "return":
+        finaloutput += "RETURN"
+        finaloutput += "\n"
+    elif term not in knownToIgnore:
         num = re.search(NUMBER, term)
         ident = re.search(IDENT, term)
         if ident:
-            finaloutput += "IDENT: "
+            finaloutput += "IDENT:"
             finaloutput += term
             finaloutput += "\n"
         elif num:
-            finaloutput += "NUMBER: "
+            finaloutput += "NUMBER:"
             finaloutput += term
             finaloutput += "\n"
-        else:
-            print "WTF"
     else:
         finaloutput += term
         finaloutput += "\n"
